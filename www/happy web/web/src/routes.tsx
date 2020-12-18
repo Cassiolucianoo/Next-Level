8fc36512e87d82d6655477c98883f3b3918164d1
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2020-12-18 18:55:33 -0300

    routers 5%

---
+++ www/happy web/web/src/routes.tsx
@@ -0,0 +1,15 @@
+import react from 'react';
+
+import{BrowserRouter, Switch, Route}from'react-router-dom';
+
+Function Routes(){
+return(
+    <BrowserRouter>
+    <Route path="/" component = {}>
+
+    </BrowserRouter>
+);
+
+}
+
+export default Routes;
