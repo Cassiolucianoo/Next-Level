0b1d945ea169d2dcfed81fbdc1d1adca0e965261
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2020-12-21 00:57:53 -0300

    Html-adjustments

--- www/happy web/web/src/pages/OrphanagesMap.tsx
+++ www/happy web/web/src/pages/OrphanagesMap.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-
+import mapMarkerImg from '../images/map-marker.svg';
+import {Link} from 'react-router-dom';
+import{FiPlus}from'react-icons/fi';
 
 function OrphanagesMap(){
     return (
         <div id = "page-map">
             <aside>
                 <header>
-                    <img src = {mapMarkerImg} att="Happy" />
+                    <img src = {mapMarkerImg} alt ="Happy" />
+                        <h2>Escolha um orfanato no mapa</h2>
+                        <p>Muitas crianças estão esperando a sua visita :)</p>
 
                 </header>
+                <footer>
+                    <strong>São paulo</strong>
+                    <span>Sp</span>
+                </footer>
             </aside>
+            <div></div> 
+            <Link to="/app" className="create-orphanage">
+
+            </Link>
         </div>
     );
 
