1d06119039bc3324cc43fd93ea521e0ef3ede0ff
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2021-01-21 01:54:18 -0300

    including import{Map,TileLayer} from 'react-leaflet

--- www/happy web/web/src/pages/OrphanagesMap.tsx
+++ www/happy web/web/src/pages/OrphanagesMap.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import mapMarkerImg from '../images/Local.svg';
 import {Link} from 'react-router-dom';
 import{FiPlus}from'react-icons/fi';
+//import{Map,TileLayer} from 'react-leaflet';
+
+import'leaflet/dist/leaflet.css';
 
 import '../styles/pages/orphanages-map.css';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+
 
 function OrphanagesMap(){
     return (
         <div id = "page-map">
             <aside>
-                <header>
+                <header>/
                     <img src = {mapMarkerImg} alt ="Happy" />
                         <h2>Escolha um orfanato no mapa</h2>
                         <p>Muitas crianças estão esperando a sua visita :)</p>
@@ -20,7 +25,14 @@ function OrphanagesMap(){
                     <span> Sp</span>
                 </footer>
             </aside>
-            <div></div> 
+            <MapContainer
+                center={[-23.5627588,-46.4559599]}
+                zoom={15}
+                style={{ width:'100%', height:'100%'}}
+            >
+             <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+            </MapContainer>
             <Link to="/app" className="create-orphanage">
                 <FiPlus size= {32} color="#FFF"/>
 
