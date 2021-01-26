0e8fc46808dbf21e0326d5a5699a0cf1dc30f085
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2021-01-26 11:38:36 -0300

    Config db

--- www/happy web/backend/src/server.ts
+++ www/happy web/backend/src/server.ts
@@ -1,36 +1,13 @@
 import express from 'express';
-
 const app = express();
-
 app.use(express.json());
 
 
+app.get('/users', (request, response) => {
 
-//métodos 
-
-//rota = conjunto
-//recurso = usuário
-
-// métodos HTTP = GET, POST, PUT, DELETE
-
-
-// GET = busca uma informação (Lista, intem )
-// POST = Criando uma informação
-// PUT = Editando uma informação
-// DELETE = Deletando uma informação
-
-// parâmetros
-
-//query params: http://localhost:3333/users?search=cassio
-//route Params: http://localhost:3333/users/1 (indentifica um user )
-//Body: http://localhost:3333/users (indentifica um user )
-
-app.post('/users/:id', (request, response) => {
-    //console.log('Ola mundo');
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
    return response.json({message: 'Bom dia Cássio Luciano da Silva'});
 });
 
 app.listen(3333);
+
+//Drive nativo, Query builder, ORM
