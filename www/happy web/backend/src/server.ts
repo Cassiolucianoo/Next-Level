8ac6876dff02c54cecc2fc19d9c85400df3410ab
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2021-01-26 11:10:43 -0300

    Fim riutes,-http-parameters-and-methods

--- www/happy web/backend/src/server.ts
+++ www/happy web/backend/src/server.ts
@@ -2,34 +2,35 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-   // console.log('Ola mundo');
+app.use(express.json());
+
+
+
+//métodos 
+
+//rota = conjunto
+//recurso = usuário
+
+// métodos HTTP = GET, POST, PUT, DELETE
+
+
+// GET = busca uma informação (Lista, intem )
+// POST = Criando uma informação
+// PUT = Editando uma informação
+// DELETE = Deletando uma informação
+
+// parâmetros
+
+//query params: http://localhost:3333/users?search=cassio
+//route Params: http://localhost:3333/users/1 (indentifica um user )
+//Body: http://localhost:3333/users (indentifica um user )
+
+app.post('/users/:id', (request, response) => {
+    //console.log('Ola mundo');
+    console.log(request.query);
+    console.log(request.params);
+    console.log(request.body);
    return response.json({message: 'Bom dia Cássio Luciano da Silva'});
 });
 
 app.listen(3333);
-
-//req / res
-// localhost:3333
-
-//aviso informação de instalação de pacote 
-// yarn add @types/express -D
-
-//instalação do typescript -D
-//yarn add typescript -D
-
-
-//Apos instalar o typescript criar o arquivo tsconfig.json  em seguida abrir o arquivo e alterar 
-// target es2017    
-//yarn tsc --init
-
-
-//salvar e instalar um pacote chamado "pacote para ultilizar o projeto em typescript e node.js"
-// yarn add ts-node-dev -D
-
-
-//executando servidor node
-//node src/server.ts
-
-
-//json view no google crome
